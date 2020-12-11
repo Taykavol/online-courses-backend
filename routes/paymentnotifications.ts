c5379adacafb8b56f4ba3d2415db516fe5e0960d
Taykavol <mvkvol@yandex.ru>
2020-12-11 21:53:27 +0300

    'Fixed'

--- routes/paymentnotifications.ts
+++ routes/paymentnotifications.ts
@@ -4,13 +4,14 @@ import {PrismaClient} from "@prisma/client"
 const prisma = new PrismaClient()
 const app = Router();
 
-app.get('/',(req,res)=>{
-    console.log('body',req.body)
-    console.log('Payment was succeed.' )
-})
+// app.get('/',(req,res)=>{
+//     console.log('body',req.body)
+//     console.log('Payment was succeed.' )
+// })
 app.post('/',(req,res)=>{
     console.log('bodyPost',req.body)
     console.log('Payment was succeed.Post' )
+    res.status(200).send()
 })
 
 export default app;
