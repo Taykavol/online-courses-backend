bde765ee8412667c418d209d0d88a0e130d6bc7e
Taykavol <mvkvol@yandex.ru>
2020-12-12 10:36:08 +0300

    'Fixed'

--- routes/paymentnotifications.ts
+++ routes/paymentnotifications.ts
@@ -12,18 +12,40 @@ const app = Router();
 // })
 app.post('/',async (req,res)=>{
     // console.log(req)
+    
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log('ip', ip)
     const {object} = req.body
     const payment_id = object.id
     const status = object.status
+    const idempotenceKey = uuidv4();
+
     console.log('status',status , 'payment_id',payment_id )
-    // if(status=='waiting_for_capture') {
-    //     await Axios({url:`https://api.yookassa.ru/v3/payments/${payment_id}/capture`,auth:{
-    //         username:"770033",
-    //         password:"test_j9KT3bKM3pRLIa4Nxhmd-Yf6fMMhxkPyBE4xjq1NAgw"
-    //     }})
-    // }
+    if(status=='waiting_for_capture') {
+        const confirm = await Axios({
+            url:`https://api.yookassa.ru/v3/payments/${payment_id}/capture`,
+            auth:{
+                username:"770033",
+                password:"test_j9KT3bKM3pRLIa4Nxhmd-Yf6fMMhxkPyBE4xjq1NAgw"
+            }, 
+            headers:{
+                'Idempotence-Key':idempotenceKey,
+                'Content-Type': 'application/json'
+            },
+            data:{
+                amount: object.amount
+            }
+        })
+        console.log('confirm',confirm)
+        return res.status(200).send()
+    }
+
+    if(status=='succeeded') {
+
+        console.log('course was bought!')
+        // Выдать курс
+    }
+    
     // console.log('bodyPost',req.body)
     // console.log('Payment was succeed.Post' )
     res.status(200).send()
