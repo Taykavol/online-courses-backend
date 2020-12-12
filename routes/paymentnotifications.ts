c360ca3ecfbd2f80bae5fc2a156f6609a85dca3d
Taykavol <mvkvol@yandex.ru>
2020-12-12 09:49:38 +0300

    'Fixed'

--- routes/paymentnotifications.ts
+++ routes/paymentnotifications.ts
@@ -1,5 +1,7 @@
 import {  Router } from "express";
 import {PrismaClient} from "@prisma/client"
+import Axios from "axios";
+import {v4 as uuidv4} from 'uuid'
 
 const prisma = new PrismaClient()
 const app = Router();
@@ -8,9 +10,21 @@ const app = Router();
 //     console.log('body',req.body)
 //     console.log('Payment was succeed.' )
 // })
-app.post('/',(req,res)=>{
-    console.log('bodyPost',req.body)
-    console.log('Payment was succeed.Post' )
+app.post('/',async (req,res)=>{
+    console.log(req)
+    const {object} = req.body
+    const payment_id = object.id
+    const status = object.status
+    console.log('status',status , 'payment_id',payment_id )
+    // if(status=='waiting_for_capture') {
+    //     await Axios({url:`https://api.yookassa.ru/v3/payments/${payment_id}/capture`,auth:{
+    //         username:"770033",
+    //         password:"test_j9KT3bKM3pRLIa4Nxhmd-Yf6fMMhxkPyBE4xjq1NAgw"
+    //     }})
+        
+    // }
+    // console.log('bodyPost',req.body)
+    // console.log('Payment was succeed.Post' )
     res.status(200).send()
 })
 
--- routes/yandex.ts
+++ routes/yandex.ts
@@ -2,36 +2,56 @@ import {  Router } from "express";
 const yandexCheckout = require('yandex-checkout')('770033', 'test_j9KT3bKM3pRLIa4Nxhmd-Yf6fMMhxkPyBE4xjq1NAgw');
 import {PrismaClient} from "@prisma/client"
 import {v4 as uuidv4} from 'uuid'
+import Axios from "axios";
 const prisma = new PrismaClient()
 const app = Router();
 
-app.get('/payment', (req, res)=>{
+app.get('/payment', async (req, res)=>{
   console.log(req.query.token)
-  
- var idempotenceKey = uuidv4();
- console.log(idempotenceKey)
-    yandexCheckout.createPayment({
-    'payment_token':req.query.token,
-    'amount': {
-        'value': '2.00',
-        'currency': 'RUB'
+  var idempotenceKey = uuidv4();
+  const result =  await Axios({url:'https://api.yookassa.ru/v3/payments', method:"POST", 
+  auth:{
+    username:"770033",
+    password:"test_j9KT3bKM3pRLIa4Nxhmd-Yf6fMMhxkPyBE4xjq1NAgw"
+  },
+  headers:{
+    'Idempotence-Key':idempotenceKey,
+    'Content-Type': 'application/json'
+  },
+  data:{
+    payment_token:req.query.token,
+    amount: {
+        value: '2.00',
+        currency: 'RUB'
     },
-    // 'payment_method_data': {
-    //     'type': 'bank_card'
-    // },
-    'confirmation': {
-        'type': 'redirect',
-        'return_url': `${process.env.FRONTEND_URL}/learning`
+    confirmation: {
+        type: 'redirect',
+        return_url: `${process.env.FRONTEND_URL}/learning`
     }
-}, idempotenceKey)
-  .then(function(result) {
-    console.log(result,result.confirmation.confirmation_url)
-    if(result.confirmation.confirmation_url) return res.json({confirmation_url:result.confirmation.confirmation_url})
-    res.json('bad')
-  })
-  .catch(function(err) {
-    console.error(err);
-  })
+  }})
+    if( result.data.confirmation.confirmation_url) return res.json({confirmation_url:result.data.confirmation.confirmation_url})
+      res.json('bad')
+  // console.log('easy',good)
+//  console.log(idempotenceKey)
+//     yandexCheckout.createPayment({
+    // 'payment_token':req.query.token,
+    // 'amount': {
+    //     'value': '2.00',
+    //     'currency': 'RUB'
+    // },
+    // 'confirmation': {
+    //     'type': 'redirect',
+    //     'return_url': `${process.env.FRONTEND_URL}/learning`
+    // }
+// }, idempotenceKey)
+//   .then(function(result) {
+//     console.log(result,result.confirmation.confirmation_url)
+//     if(result.confirmation.confirmation_url) return res.json({confirmation_url:result.confirmation.confirmation_url})
+//     res.json('bad')
+//   })
+//   .catch(function(err) {
+//     console.error(err);
+//   })
 })
 
 export default app;
